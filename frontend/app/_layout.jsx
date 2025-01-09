74bcee4b4c30fd84f8346aa90ba8e8ac35f58ac6
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2025-01-09 23:26:17 +0100

    Initial frontend commit

---
+++ frontend/app/_layout.jsx
@@ -0,0 +1,20 @@
+import { SplashScreen, Stack } from 'expo-router'
+import '../global.css'
+import GlobalProvider from '../context/GlobalProvider'
+
+export const API_URL = 'http://192.168.0.69:5000/api'
+
+const RootLayout = () => {
+    
+  return (
+    <GlobalProvider>
+        <Stack>
+          <Stack.Screen name='index' options={{headerShown: false}} />
+          <Stack.Screen name='(auth)' options={{headerShown: false}} />
+          <Stack.Screen name='(tabs)' options={{headerShown: false}} />
+        </Stack>
+    </GlobalProvider>
+  )
+}
+
+export default RootLayout
---
+++ frontend/app/index.jsx
@@ -0,0 +1,27 @@
+import { View, Text, ScrollView, Button } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '../components/CustomButton'
+import { router } from 'expo-router'
+
+const App = () => {
+  return (
+    <SafeAreaView className='h-full bg-slate-100'>
+        <ScrollView contentContainerStyle={{height: '100%'}}>
+            <View className='w-full items-center justify-center h-full'>
+                <CustomButton
+                    title='Sign in'
+                    handlePress={() => router.push('/sign-in')}
+                />
+                <Text className='text-slate-500 font-semibold text-xl my-3'>or</Text>
+                <CustomButton
+                    title='Continue as guest'
+                    handlePress={() => router.push('/home')}
+                />
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
